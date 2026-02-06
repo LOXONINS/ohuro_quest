d1d19652a1d5c926724dbe177eeaf3a31bfb2ff6
大森さくら <oomorisakura@oomorisakuranoMacBook-Air.local>
2026-02-06 23:08:43 +0900

    fix:rubocopが通るよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  before_action :configure_permitted_parameters, if: :devise_controller?  
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   allow_browser versions: :modern
 
@@ -10,5 +10,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
-
 end
