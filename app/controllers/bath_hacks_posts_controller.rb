1d447adc9ed0616af3ecb7a447038d73e82951ed
大森さくら <oomorisakura@oomorisakuranoMacBook-Air.local>
2026-02-07 12:20:31 +0900

    add:新規投稿画面作った

--- app/controllers/bath_hacks_posts_controller.rb
+++ app/controllers/bath_hacks_posts_controller.rb
@@ -3,4 +3,8 @@ class BathHacksPostsController < ApplicationController
   def index
     @bath_hacks_posts = BathHacksPost.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @bath_hacks_posts = BathHacksPost.new
+  end
 end
