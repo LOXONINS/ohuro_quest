3bcb36c09d52ad88149295b79c6c67cd9ebd50ac
大森さくら <oomorisakura@oomorisakuranoMacBook-Air.local>
2026-02-07 13:03:04 +0900

    add:新規投稿機能・ログインなしユーザーはtopページと投稿一覧ページのみ閲覧可能にした

--- app/controllers/bath_hacks_posts_controller.rb
+++ app/controllers/bath_hacks_posts_controller.rb
@@ -1,4 +1,5 @@
 class BathHacksPostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
 
   def index
     @bath_hacks_posts = BathHacksPost.includes(:user).order(created_at: :desc)
@@ -6,5 +7,21 @@ class BathHacksPostsController < ApplicationController
 
   def new
     @bath_hacks_post = BathHacksPost.new
+  end
+
+  def create
+	@bath_hacks_posts = current_user.bath_hacks_posts.build(bath_hacks_post_params)
+	if @bath_hacks_posts.save
+		redirect_to bath_hacks_posts_path, success: "投稿できました"
+	else
+		flash.now[:alart] = "投稿できませんでした"
+		render :new, status: :unprocessable_entity
+	end
+  end
+
+  private
+
+  def bath_hacks_post_params
+    params.require(:bath_hacks_post).permit(:title, :content)
   end
 end
